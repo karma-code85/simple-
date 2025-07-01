7972e8902d73cd6dbc993846ef7bcbb7d0256a83
Marwan Mohamed <ranmoh85@gmail>
2025-07-01 11:27:08 +0200

    the 2th edit

--- scrip.js
+++ scrip.js
@@ -1,11 +1,12 @@
 const btn=document.getElementById('btn')
 const content=document.getElementById('bass')
 const sentMessage=document.getElementById('sent-text')
-console.log(sentMessage)
+// console.log(sentMessage)
 btn.addEventListener(('click'),  () => {
     const messagArea=sentMessage.value.trim();
-    if(messagArea !== ""){
-        const createDiv=document.createElement('div')
+    const createDiv=document.createElement('div')
+    if(messagArea !== "")
+        {
         createDiv.classList.add('message', "sent")
         createDiv.textContent= messagArea;
     }
